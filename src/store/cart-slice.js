1b51eba65b9d62f09691cf5106948aac1e7da49c
Daphne <daphneruth304@gmail.com>
2022-07-19 14:11:26 +0300

    removing items from the cart

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -27,6 +27,18 @@ createSlice({
         
 
        },
-       removeItemToCart(state, action){}
+       removeItemToCart(state, action){
+       const id = action.payload;
+       const existingItem = state.items.find(item => item.id ===id);
+       if(existingItem.quantity === 1){
+        state.items =state.items.filter(item=>item.id !==id)
+
+       }else{
+        existingItem.quantity--;
+        existingItem.totalPrice =existingItem.totalPrice - existingItem.price;
+       }
+
+
+       }
     }
 });
